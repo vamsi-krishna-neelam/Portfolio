a87b62e8f415a457d5e35ba7217cb2c004c3b424
Aslam554 <begaslam405@gmail.com>
2025-04-16 15:06:15 +0530

    re branded and designed my portfolio website

--- src/components/ProjectBox.jsx
+++ src/components/ProjectBox.jsx
@@ -1,20 +1,41 @@
-export default function EducationBox({ logo, name, year, branch, grade }) {
+export default function ProjectBox({ pic, title, desc, github, website }) {
   return (
-    <div className="flex flex-col lg:flex-row border bg-gradient-to-r from-black to-gray-900 text-white border-black hover:border-teal-300 hover:translate-x-[-10px] hover:transition-all hover:duration-300 hover:ease-in-out rounded-xl px-6 py-6 mb-6 shadow-xl transform w-full lg:w-[90%] xl:w-[80%]">
-      <div className="bg-black text-white flex justify-center items-center w-full lg:w-1/5 mb-4 lg:mb-0">
-        <img src={logo} className="w-24 lg:w-32" alt={`${name} logo`} />
+    <div
+      className="border border-gray-500 shadow-lg rounded-lg  duration-500 hover:ease-in-out border-2 border-white/10 hover:border-teal-300 bg-black text-white p-2 overflow-hidden"
+      style={{ width: "400px", height: "500px" }} // Set fixed width and height
+    >
+      <div>
+        <img
+          src={"projectsnap/" + pic}
+          alt=""
+          className="w-full h-48 md:h-56 lg:h-64 object-contain"
+        />
       </div>
-      <div className="bg-black text-white py-6 lg:py-8 pl-8 lg:pl-12 w-full lg:w-4/5">
-        <p className="text-2xl lg:text-3xl mb-3 lg:mb-5 font-semibold tracking-wide">{name}</p>
-        <div className="flex flex-col lg:flex-row justify-between w-full mb-4">
-          <p className="text-lg lg:text-xl font-medium opacity-80 mb-2 lg:mb-0">{branch}</p>
-          <p className="text-lg lg:text-xl font-medium opacity-80 mb-2 lg:mb-0">{year}</p>
-        </div>
-        <div className="flex items-center">
-          <p className="text-lg lg:text-xl font-medium opacity-80">
-            {grade <= 10 ? `CGPA: ${grade}` : `Percentage: ${grade}%`}
-          </p>
-        </div>
+      <div className="p-4">
+        <p className="text-xl md:text-xl raleway-bold mb-4">{title}</p>
+        <p className="text-base md:text-[12px] text-gray-300 work-sans-regular mb-4 uppercase">
+          {desc}
+        </p>
+      </div>
+      <div className="p-4 pt-0 mb-4 flex flex-col md:flex-row gap-4">
+        <a
+          href={github}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-blue-600 hover:bg-blue-900 text-white px-4 py-2 rounded-lg text-center"
+        >
+          Github
+        </a>
+        {website && (
+          <a
+            href={website}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="bg-teal-700 hover:bg-teal-900 text-white px-4 py-2 rounded-lg text-center"
+          >
+            Website
+          </a>
+        )}
       </div>
     </div>
   );
